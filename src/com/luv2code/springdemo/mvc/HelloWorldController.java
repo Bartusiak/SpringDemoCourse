18b5ae72a4a120489d9f3bd629504b33e206e0df
Bartusiak <bsk69@o2.pl>
2020-02-26 00:59:50 +0100

    Added processFormThree

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -33,4 +34,18 @@ public class HelloWorldController {
 
         return "helloworld";
     }
+
+    @RequestMapping("/processFormThree")
+    public String processFormThree(@RequestParam("studentName") String theName, Model model){
+
+        //convert the data to all caps
+        theName = theName.toUpperCase();
+        //create the msg
+        String result = "Yo! "+theName;
+        //add msg to the model
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
 }
